dc5d988f2d8f20fefe5e40f2a507e9ad142c9884
Emiliano Spada <emilianos13@gmail.com>
2023-06-23 00:59:22 +0200

    vault db secrets engine integration

--- init.sql
+++ init.sql
@@ -1,13 +1,14 @@
 -- Create asterisk database
---CREATE DATABASE asterisk;
+CREATE DATABASE asterisk;
 
--- Grant privileges to freepbxuser for asterisk database
---GRANT ALL PRIVILEGES ON `asterisk`.* TO 'freepbxuser'@'%';
+CREATE DATABASE asteriskcdrdb;
 
---CREATE DATABASE asteriskcdrdb;
+GRANT ALL PRIVILEGES ON *.* TO 'vault'@'172.18.0.5';
 
 -- Grant privileges to freepbxuser for asteriskcdrdb database
 --GRANT ALL PRIVILEGES ON `asteriskcdrdb`.* TO 'freepbxuser'@'%';
 
-GRANT ALL PRIVILEGES ON *.* TO 'vault'@'172.18.0.5';
+-- Grant privileges to freepbxuser for asterisk database
+--GRANT ALL PRIVILEGES ON `asterisk`.* TO 'freepbxuser'@'%';
+
 FLUSH PRIVILEGES;
--- vault/configure.sh
+++ vault/configure.sh
@@ -1,8 +1,20 @@
 vault token create -policy=administrator-policy
 vault secrets enable database
-vault write database/config/mysql-db \
+vault write database/config/mysql-database \
     plugin_name=mysql-database-plugin \
     connection_url="{{username}}:{{password}}@tcp(172.18.0.2:3306)/" \
-    allowed_roles="mysql-role" \
+    allowed_roles="*" \
     username="vault" \
     password="vault_password"
+
+vault write database/roles/asterisk-role \
+    db_name=asterisk \
+    creation_statements="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL PRIVILEGES ON `asterisk`.* TO '{{name}}'@'%';" \
+    default_ttl="1h" \
+    max_ttl="24h"
+
+vault write database/roles/asteriskcdrdb-role \
+    db_name=asteriskcdrdb \
+    creation_statements="CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL PRIVILEGES ON `asteriskcdrdb`.* TO '{{name}}'@'%';" \
+    default_ttl="1h" \
+    max_ttl="24h"
