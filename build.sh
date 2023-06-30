eaf3e96315dd46bd5aa7c372e97868f9a5d7ac42
escomputers <emilianos13@gmail.com>
2023-06-30 09:44:18 +0000

    added docker installation to build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,24 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         sudo apt install -y jq
     fi
 
+    # Check if Docker is installed
+    if ! command -v docker &> /dev/null; then
+        echo "Docker is not installed. Installing..."
+        sudo apt-get update
+        sudo apt-get install \
+            ca-certificates \
+            curl \
+            gnupg
+        sudo mkdir -m 0755 -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        echo \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    fi
+
     # BUILD
     cd vault-transit && docker build -t vault-transit:custom .
     cd .. && docker compose up -d --build
