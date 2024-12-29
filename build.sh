90fdbe2449f3a7d40769770787665ca9a9af48fd
emilianospada <emilianos13@gmail.com>
2024-12-29 01:55:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,9 @@
 freepbxip="172.18.0.20"
 network_interface="freepbxint"
 
-# DOCKER INSTALL
-if [[ "$*" == *"--install-docker"* ]]; then
-    sudo apt-get update
-    sudo apt-get install \
-        ca-certificates \
-        curl \
-        gnupg
-    sudo mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # INSTALL FREEPBX
-elif [[  "$*" == *"--install-freepbx"*  ]]; then
+if [[  "$*" == *"--install-freepbx"*  ]]; then
     docker compose exec -it freepbx php /usr/src/freepbx/install -n --dbuser=freepbxuser --dbpass="$(cat freepbxuser_password.txt)" --dbhost=db
 
 # CLEAN
